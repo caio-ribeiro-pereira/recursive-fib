b5bd2d0b91e6fd76c26e83f969925f0bcf3e787c
leocavalcante <lc@leocavalcante.com>
2020-04-24 20:06:57 -0300

    Add PHP

---
+++ php/fib.php
@@ -0,0 +1,22 @@
+<?php
+
+function fibonacci($n)
+{
+    if ($n <= 1) {
+        return $n;
+    }
+
+    return fibonacci($n - 1) + fibonacci($n - 2);
+}
+
+try {
+    $sequences = intval($argv[1]);
+    $start = hrtime(true);
+    $output = fibonacci($sequences);
+    $end = hrtime(true);
+    $elapsed = ($end - $start) / 1000000;
+
+    echo "[PHP]\nFib: {$sequences}x | Result: {$output} | Time: {$elapsed}ms";
+} catch (Throwable $exception) {
+    echo "[PHP] Error: {$exception->getMessage()}";
+}
