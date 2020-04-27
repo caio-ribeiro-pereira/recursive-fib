09b24399c5723c4909be3c816884e0436074c7e0
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2020-04-27 19:54:06 -0300

    fix: php fib syntax and tab space

--- php/fib.php
+++ php/fib.php
@@ -1,22 +1,18 @@
 <?php
-
-function fibonacci($n)
-{
-    if ($n <= 1) {
-        return $n;
-    }
-
-    return fibonacci($n - 1) + fibonacci($n - 2);
+function fibonacci($n) {
+  if ($n <= 1) {
+    return $n;
+  }
+  return fibonacci($n - 1) + fibonacci($n - 2);
 }
 
 try {
-    $sequences = intval($argv[1]);
-    $start = hrtime(true);
-    $output = fibonacci($sequences);
-    $end = hrtime(true);
-    $elapsed = round(($end - $start) / 1000000);
-
-    echo "[PHP]\nFib: {$sequences}x | Result: {$output} | Time: {$elapsed}ms";
+  $sequences = intval($argv[1]);
+  $start = hrtime(true);
+  $output = fibonacci($sequences);
+  $end = hrtime(true);
+  $elapsed = intval(round(($end - $start) / 1000000));
+  echo "[PHP]\nFib: {$sequences}x | Result: {$output} | Time: {$elapsed}ms";
 } catch (Throwable $exception) {
-    echo "[PHP] Error: {$exception->getMessage()}";
+  echo "[PHP] Invalid argument";
 }
